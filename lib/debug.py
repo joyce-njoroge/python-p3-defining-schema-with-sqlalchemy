133e75e56a9903d07a27d8a21a3b6ea75511f60a
Joyce Njoroge <joycewambuku33@gmail.com>
2023-06-01 13:15:27 +0300

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -3,8 +3,10 @@
 from sqlalchemy import create_engine
 
 from sqlalchemy_sandbox import Student
+from sqlalchemy_sandbox import Base
 
 engine = create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
